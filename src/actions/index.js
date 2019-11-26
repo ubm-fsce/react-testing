a855057fcb119557dcbde2a7ab8c72a3cada16a6
Udaya Bhasker Mattupalli <uday1005@gmail.com>
2019-11-25 23:50:20 -0800

    20191125 Commit Message 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 import axios from 'axios';
 
 
@@ -14,5 +14,13 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: response
-  }
+  };
 }
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
+  };
+}
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
-export default () => {
+import { Route, Link } from 'react-router-dom';
 
-  return (
-    <div>
-      <div> <CommentBox /> </div>
-      <div> <CommentList /> </div>
-    </ div >
-  );
-};
+
+class App extends Component {
+
+  renderButton() {
+
+  }
+  renderHeader() {
+    return (
+      <ul>
+        <li> <Link to='/'>Home</Link></li>
+        <li><Link to='/post'> Post comment</Link></li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </ div >
+    );
+  }
+}
+
+export default App;
